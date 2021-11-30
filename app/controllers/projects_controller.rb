5acdcc7e94a1f6e4c1fcfc3ebac5e6c01ebbfd30
ehrndz <eherndz23@gmail.com>
2021-11-30 01:54:40 +0000

    Added methods to Log in and Sign Up buttons, also added Log Out button

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,7 +9,7 @@ class ProjectsController < ApplicationController
   
   before_action :set_project, only: %i[ show edit update destroy ]
 
-  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, :raise => false
 
   # GET /projects or /projects.json
   def index
